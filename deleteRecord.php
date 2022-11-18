8df3c2df64da33735f7260654b9b803930f38634
cw353 <claire@claires-air-5.student.wheaton.edu>
2022-11-17 21:59:54 -0600

    implemented delete functionality

--- deleteRecord.php
+++ deleteRecord.php
@@ -4,7 +4,7 @@
 	require_once 'helperFunctions.php';
 	require_once 'tables.php';
 
-	//$mysqli = new mysqli('localhost', 'root', 'root', 'project');
+	$mysqli = new mysqli('localhost', 'root', 'root', 'project');
 	$tablename = $_GET['table'];
 	$table = $tables[$tablename];
 
@@ -21,22 +21,29 @@
 
 	$query = 'delete from ' . $table->getName() . ' where ' . join(' and ', $filter_expr);
 
-	echo $query;
-	echo " - filter_var: " . join(', ', $filter_var);
-
-	//$result = getQueryResult($mysqli, $query, $filter_var, $filter_types);*/
+	$num_affected = executeQueryGetAffected($mysqli, $query, $filter_var, $filter_types);
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Modify Record</title>
+		<title>Delete Record</title>
 	</head>
 
 	<body>
-
-		<br>
-		<br>
+		<?php
+			if ($num_affected < 0) {
+				echo "<p>An error occurred and the operation could not be completed.</p>";
+			} else {
+				echo "<p>The operation has succeeded. $num_affected record";
+				echo $num_affected === 1 ? ' has' : 's have';
+				echo ' been deleted. Any other records that reference this record have also been deleted as necessary.</p>';
+			}
+		?>
+		<form method="get" action="viewFilteredRecords.php">
+			<input type="hidden" name="table" value="<?php echo sanitizeHtml($tablename); ?>">
+			<button type="submit">View Results</button>
+		</form>
 		<form method="post" action="chooseTable.php">
 			<button type="submit">Filter Records for Another Table</button>
 		</form>
@@ -45,6 +52,5 @@
 </html>
 
 <?php
-	/*$result && $result->free();
-	$mysqli && $mysqli->close();*/
+	$mysqli && $mysqli->close();
 ?>
